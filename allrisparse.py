a778d9e17b8e260dbe5f165bcbcedb729dc600a2
Corny <Corny@Corneliuss-MacBook-Pro.local>
2013-03-28 23:44:45 +0100

    Ausleseverfahren geändert, Fehler behoben

--- allrisparse.py
+++ allrisparse.py
@@ -34,92 +34,78 @@ def antragparsen(html):
 #parst den Antrag nach Antragstext und Begründug
 #benoetigt die HTML datei der DRS
 def antragstext(soup):
-        spans = soup.find_all('span')
-        
-        i = 0
-
-        moegebeschliessen = "Die Bezirksverordnetenversammlung Treptow-Köpenick von Berlin möge beschließen:"
         antragtext = ""
-        
-        begruendungbool = False
-        begruendung = "Begründung:"
         begruendungstext = ""
-        
         ueberweisungstext = ""
-        ueberweisung =  ""
-        ueberweisungsbool = False
-        
-        drsbool = False
-        
+        antragtext = ""
         ausschussbeschluss = ""
-        ausschussbeschlussbool = False
-        
         beschluss = ""
-        beschlussbool = False
         
-        #for span in spans:
-        #    print span
-        
+        text = ""
+        spans = soup.find_all('span')
         for span in spans:
-                print span
-                #print span.contents[0]
-                text = unicode(span.contents[0])
-                
-                if text[0:75] == u"Die Bezirksverordnetenversammlung Treptow-Köpenick von Berlin beschließt:" or beschlussbool == True:
-                        beschlussbool = True
-                        beschluss = beschluss + text
-                
-                elif text == u"Der Ausschuss für " or ausschussbeschlussbool == True:
-                        ausschussbeschlussbool = True
-                        if text[0:8] == u"<a href=":
-                                ausschussbeschluss = ausschussbeschluss + unicode(span.a.contents[0])
-                        else:
-                                ausschussbeschluss = ausschussbeschluss + text
+            #print span
+            #print unicode(span.contents[0])
+            text = text + unicode(span.contents[0])
+            a = span.find('a')
+            if a != None:
+                #print "!!!!!!!!!"
+                text = text + unicode(a.contents[0])
+                try:
+                        text = text + unicode(span.contents[2])
+                except:
+                        pass
+            #print text
+        link = True
+        while link == True:
+                if "<a href=" in text:
+                        linkanfang = text.find(u"<a href=")
+                        linkende = text.find(u"</a>")
+                        text = text[:linkanfang] + text[linkende+4:]
+                else:
+                        link = False
+        print text
+        antraganfang = text.find(u"Die Bezirksverordnetenversammlung Treptow-Köpenick von Berlin möge beschließen:")
+        antragende = text.find(u"Begründung:")
+        antragtext = text[antraganfang:antragende]
+        text = text[antragende:]
+        #print text
+        
+        begruendunganfag = text.find(u"Begründung:")
+        begruendungende = text.find(u"In der Sitzung der BVV am")
+        #print begruendunganfag
+        #print begruendungende
+        begruendungstext = text[begruendunganfag:begruendungende]
+        text = text[begruendungende:]
+        #print text
+        
+        ueberweisunganfang = text.find(u"In der Sitzung der BVV am")
+        ueberweisungende = text.find(u"überwiesen:") + len("überwiesen:")
+        #print ueberweisunganfang
+        #print ueberweisungende
+        ueberweisungstext = text[ueberweisunganfang:ueberweisungende]
+        text = text[ueberweisungende:]
+        #print text
+              
+        ausschussanfang = text.find(u"Der Ausschuss für ")
+        ausschussende = text.find(u"Die Bezirksverordnetenversammlung Treptow-Köpenick von Berlin beschließt:")
+        #print ausschussanfang
+        #print ausschussende
+        ausschussbeschluss = text[ausschussanfang:ausschussende]
+        text = text[ausschussende:]
+        #print text        
+        
+        beschlussanfang = text.find(u"Die Bezirksverordnetenversammlung Treptow-Köpenick von Berlin beschließt:")
+        #print ausschussanfang
+        #print ausschussende
+        beschluss = text[beschlussanfang:]
+        #print text                        
                         
-                elif text == u"Drs. VII/" or drsbool == True:
-                        drsbool = True
-                
-                elif text[0:8] == u"<a href=":
-                        #print span.a.contents[0]
-                        ueberweisungstext = ueberweisungstext + unicode(span.a.contents[0])
-                
-                elif ueberweisungsbool == True:
-                        ueberweisungstext = ueberweisungstext + unicode(span.contents[0])                  
-                
-                elif text[0:26] == u"In der Sitzung der BVV am ":
-                        ueberweisungsbool = True
-                        ueberweisungstext = ueberweisungstext + unicode(span.contents[0])
                         
-                        
-                elif begruendungbool == True and ueberweisungsbool == False:
-                        begruendungstext = begruendungstext + unicode(span.contents[0])           
-                
-                elif text == u"ndung: ":
-                        begruendungbool = True
-                        span.decompose()
-                               
-                elif text == u"Begründung: ":
-                        begruendungbool = True
-                        
-                elif span.contents[0] == u"Begründung:":
-                        begruendungbool = True
-                
-                #if span.contents[0] == u"In der Sitzung der BVV am ":
-                        #ueberweisungsbool = True
-                        #ueberweisungstext = ueberweisungstext + span.contents[0]
-                        
-                elif i>7 and text != u" " and begruendungbool == False and ueberweisungsbool == False:
-                        antragtext = antragtext + unicode(span.contents[0])                
-                i += 1
-                
-                        
-                        
-        print moegebeschliessen
+       
         print antragtext
-        print begruendung
         print begruendungstext
         print ueberweisungstext
-        #print drstext
         print antragtext
         print ausschussbeschluss
         print beschluss

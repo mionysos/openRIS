764ba36fda774d5eb457046256ffb04b0480f0c7
Mionysos <d.mitterneder@rediska.de>
2013-03-29 09:26:05 +0100

    Create parameter.py

---
+++ parameter.py
@@ -0,0 +1,41 @@
+bezirke_in_url = [
+		"ba-treptow-koepenick",
+		"ba-tempelhof-schoeneberg",
+		"ba-steglitz-zehlendorf",
+		"ba-spandau",
+		"ba-reinickendorf",
+		"ba-pankow",
+		"ba-neukoelln",
+		"ba-mitte",
+		"ba-marzahn-hellersdorf",
+		"ba-lichtenberg",
+		"ba-friedrichshain-kreuzberg",
+		"ba-charlottenburg-wilmersdorf"]
+		
+bezirke = [
+		 "Treptow-Köpenick",
+		 "Tempelhof-Schöneberg",
+		 "Steglitz-Zehlendorf",
+		 "Spandau",
+		 "Reinickendorf",
+		 "Pankow	",
+		 "Neukölln",
+		 "Mitte",
+		 "Marzahn-Hellersdorf	",	
+		 "Lichtenberg",
+		 "Friedrichshain-Kreuzberg",
+		 "Charlottenburg-Wilmersdorf"]
+
+scripe_in_url = [
+		"vo020.asp",
+		"vo040.asp", 				# Drucksachen: Name, Initiator, Abschl, D.-Art
+		"pa021.asp", 				# aktuelle BV: Name, Art der Mitarbeit, Herkunft, seit
+		"kp020.asp",
+		"au010.asp", 				# Ausschuesse: Name, Mitglieder, Letzte Sitzung, Naechste Sitzung 
+		"au020.asp",
+		"fr010.asp", 				# Fraktionen: Name, Mitglieder, Letzte Sitzng, Naechste Sitzung
+		"fr020.asp",
+		"to010.asp",				# Tagesordnung eines to010.asp?SILFDNR=1058&options=4
+		"ka040.asp?showall=true", 	# Kleine Anfragen: Nummer, Betreff, Eingang, Antwort
+		"si010.asp", 				# Sitzungskalender
+		"si018.asp"] 				# Sitzung Suchen
